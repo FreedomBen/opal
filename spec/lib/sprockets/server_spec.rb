6b7a7f9eb05a0fa6fca3e47b30fbba044f8577df
Elia Schito <elia@schito.me>
2015-03-04 17:39:40 +0100

    WIP fixing source maps server spec

--- spec/lib/sprockets/server_spec.rb
+++ spec/lib/sprockets/server_spec.rb
@@ -10,7 +10,7 @@ describe Opal::Server do
       s.main = 'opal'
       s.debug = false
       s.append_path File.expand_path('../../fixtures', __FILE__)
-      s.sprockets.logger = Logger.new(nil)
+      s.sprockets.logger = Logger.new(STDOUT)
     }
   end
 
@@ -24,46 +24,55 @@ describe Opal::Server do
       get '/assets/source_map.js'
       expect(last_response).to be_ok
 
-      get '/assets/source_map.map'
+      get '/assets-maps/source_map.map'
       expect(last_response).to be_ok
     end
 
     it 'serves map on a subfolder file' do
       js_path = '/assets/source_map/subfolder/other_file.js'
-      map_path = '/assets/source_map/subfolder/other_file.map'
+      map_path = '/assets-maps/source_map/subfolder/other_file.map'
 
       get js_path
 
       expect(last_response).to be_ok
-      received_map_path = extract_linked_map(last_response.body)
+      received_map_path = extract_map_path(last_response)
       expect(expand_path(received_map_path, js_path+'/..')).to eq(map_path)
 
-      get '/assets/source_map/subfolder/other_file.map'
+      get '/assets-maps/source_map/subfolder/other_file.map'
       expect(last_response).to be_ok
     end
 
     it 'serves map on a subfolder file' do
       js_path = '/assets/source_map/subfolder/other_file.js'
-      map_path = '/assets/source_map/subfolder/other_file.map'
+      map_path = '/assets-maps/source_map/subfolder/other_file.map'
 
       get js_path
 
       expect(last_response).to be_ok
-      received_map_path = extract_linked_map(last_response.body)
+      received_map_path = extract_map_path(last_response)
       expect(expand_path(received_map_path, js_path+'/..')).to eq(map_path)
 
 
-      get '/assets/source_map/subfolder/other_file.map'
+      get '/assets-maps/source_map/subfolder/other_file.map'
       expect(last_response).to be_ok
       map = ::SourceMap::Map.from_json(last_response.body)
-      expect(map.sources).to include('/assets/source_map/subfolder/other_file.rb')
+      expect(map.sources).to include('/assets-maps/source_map/subfolder/other_file.rb')
     end
   end
 
-  def extract_linked_map(body)
+  def extract_map_path(response)
     source_map_comment_regexp = %r{//# sourceMappingURL=(.*)$}
-    expect(body).to match(source_map_comment_regexp)
-    body.scan(source_map_comment_regexp).first.first
+
+    case
+    when response.body =~ source_map_comment_regexp
+      p :body
+      body.scan(source_map_comment_regexp).first.first
+    when response.headers['X-SourceMap']
+      p :header
+      response.headers['X-SourceMap']
+    else
+      raise "cannot find source map in response: #{response.inspect}"
+    end
   end
 
   def expand_path(file_name, dir_string)
